a1a774d25dfda2659a8ef542d56c3ae7ff93f932
morimolymoly <morimolymoly@gmail.com>
2022-07-17 12:11:53 +0900

    se0 kun upate

---
+++ dcdc.js
@@ -0,0 +1,17 @@
+const dcdc = (param) => {
+    let dcdc = document.querySelector(param.dcdc);
+    let speed = param.speed;
+    let message = param.message.split("");
+
+    message.forEach((char, index) => {
+        setTimeout(() => {
+            dcdc.textContent += char;
+        }, speed * index);
+    });
+};
+
+dcdc({
+    dcdc: "#dcdc",
+    speed: 70,
+    message: "We Are DCDC Japan based Security Team!!!"
+});
